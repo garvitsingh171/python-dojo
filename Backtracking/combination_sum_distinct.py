f8108138b28f74565b15218f2b2f328ff89c86f2
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-03-20 14:37:30 +0530

    combination_sum.py

--- Backtracking/combination_sum_distinct.py
+++ Backtracking/combination_sum_distinct.py
@@ -1,13 +1,24 @@
-def combination(nums):
+def combination(nums, target):
     result = []
 
-    def backtrack(path):
-        pass
+    def backtrack(start, path, target):
+        if target == 0:
+            result.append(path[:])
+            return
 
-    backtrack([])
+        if target < 0:
+            return
+
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i, path, target - nums[i])
+            path.pop()
+
+    backtrack(0, [], target)
     return result
 
 
 arr = list(map(int, input().split()))
+target = int(input())
 
-print(combination(arr))
+print(combination(arr, target))
