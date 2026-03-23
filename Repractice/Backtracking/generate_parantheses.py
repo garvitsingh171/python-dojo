90347e853649b1ff1369230153f9c3fdcdfc45d0
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-03-23 10:01:31 +0530

    repractice generate parantheses

---
+++ Repractice/Backtracking/generate_parantheses.py
@@ -0,0 +1,21 @@
+def parantheses(n):
+    result = []
+
+    def backtrack(path, open_count, close_count):
+        if len(path) == n * 2:
+            result.append(path)
+            return
+
+        if open_count < n:
+            backtrack(path + '(', open_count+1, close_count)
+
+        if close_count < open_count:
+            backtrack(path + ')', open_count, close_count+1)
+
+    backtrack('', 0, 0)
+    return result
+
+
+n = int(input())
+
+print(parantheses(n))
