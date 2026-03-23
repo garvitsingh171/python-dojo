0b684ab4f3cc2cfc96f740e2718b475d3cb439af
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-03-23 09:32:08 +0530

    repractice subset-2

--- Repractice/Backtracking/subset-2.py
+++ Repractice/Backtracking/subset-2.py
@@ -1,15 +1,18 @@
 def subset(arr):
     result = []
 
-    def backtrack(path, start):
+    def backtrack(start, path):
         result.append(path[:])
 
         for i in range(start, len(arr)):
+            if i > 0 and arr[i] == arr[i-1]:
+                continue
+
             path.append(arr[i])
-            backtrack(path, i+1)
+            backtrack(i+1, path)
             path.pop()
 
-    backtrack([], 0)
+    backtrack(0, [])
     return result
 
 
